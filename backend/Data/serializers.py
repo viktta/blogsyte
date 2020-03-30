6506cd239ee5c16ebe0818a98aed08b7967cc87b
viktta <45240747+viktta@users.noreply.github.com>
2020-03-30 13:41:42 -0700

    ooops

--- backend/Data/serializers.py
+++
@@ -1,8 +0,0 @@
-from . import models
-from rest_framework import serializers
-
-
-class PostsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = models.Posts
-        fields = '__all__'
